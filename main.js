09d086f2fc4e1018b75bbfec4b06aeae8bbdaf95
florian60430 <florian.wantelez@hotmail.com>
2021-01-29 14:48:49 +0100

    calcule et affichage des points

--- main.js
+++ main.js
@@ -59,3 +59,126 @@ let indiceReponseUser = 0;
 let scoreUser = 0;
 
 let reponseUser = new Array();
+
+
+const main = () => 
+{
+
+
+     /* --------------------------------- 
+    RECUPERATION DES ELEMENTS DU DOM 
+    -----------------------------------*/
+    
+    //question
+    const question = document.getElementById("question");
+    
+    //formulaire
+    const propose = document.getElementById("propose");
+    
+    //bouton
+    const btnValider = document.getElementById("btn");
+
+    //score 
+    const score = document.getElementById("score");
+
+    //questionParent
+    const questionParent = document.getElementById("questionParent");
+
+    /* -----------------------
+     INITIALISATION DE LA PAGE
+     ------------------------ */
+
+     
+    //1ère question on load
+    document.addEventListener('DOMContentLoaded', function (e) {
+
+        //1ère question du tableau
+        question.innerHTML = tabQuestion[0].getQuestion();
+        //1ère propositions du tableau
+        propose.innerHTML = createForm(tabQuestion[0].getProposition());
+    });
+
+
+    /* ------------------------------
+    GESTION DES QUESTION ET REPONSE
+    -------------------------------*/
+    
+
+    // change la question et la reponse a chaque click du formulaire
+    btnValider.addEventListener('click', function (e) {
+
+        //annuler le comportement normal du formulaire
+        e.preventDefault();
+
+        
+        if (tabQuestion[i] != null) 
+        {
+            //changement de la question
+            question.innerHTML = tabQuestion[i].getQuestion();
+
+            if(tabQuestion[i].getProposition() != null)
+            {
+                propose.innerHTML = createForm(tabQuestion[i].getProposition());
+                console.log(tabQuestion[i]);
+            }
+            //sinon un input texte
+            else 
+            {
+                propose.innerHTML = "<input type='text' name='test' id='reponse'>";
+            }
+
+            i++;
+            indiceReponseUser++;
+        }
+        else 
+        {
+            /* -------------------------
+            CALCULE DU SCORE DU USER
+            ------------------------- */
+
+            for (i = 0; i<tabQuestion.length; i++) 
+            {
+                if(tabQuestion[i].getReponse() == reponseUser[i])
+                {
+                    scoreUser += 1;
+                }
+                else 
+                {
+                    scoreUser -= 1;
+                }
+            }
+            //affichage du score du user
+            question.remove();
+            questionParent.remove();
+            propose.innerHTML = "score : "+ scoreUser;
+            btnValider.remove();
+
+        }
+    });
+
+    //génération des bouton input réponse
+    const createForm = (proposeIt) => {
+
+        let i;
+        let form;
+        let chaineFinale = "";
+
+        for (i = 0; i < proposeIt.length; i++) 
+        {
+            form = "<input type='radio' id='reponse" + i + "' name='test' value='" + proposeIt[i] + "'> <label for='reponse" + i + "'>" + proposeIt[i] + "</label>";
+            chaineFinale = chaineFinale + form;
+        }
+    
+        return chaineFinale;
+
+    }
+
+    /* ------------------------------
+    ENREGISTREMENT DES REPONSES USER
+    ------------------------------- */
+
+    propose.addEventListener('input', function(e)
+    {
+        reponseUser[indiceReponseUser] = e.target.value;
+    });  
+}
